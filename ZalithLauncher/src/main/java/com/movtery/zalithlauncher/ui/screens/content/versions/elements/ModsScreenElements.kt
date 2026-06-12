8f52ae59e2368524aa6f7cf65f3458b17ddca4de
movte <movtery228@qq.com>
2026-06-12 16:20:56 +0800

    chore: 调整列表项 UI 布局

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/versions/elements/ModsScreenElements.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/versions/elements/ModsScreenElements.kt
@@ -19,17 +19,14 @@
 package com.movtery.zalithlauncher.ui.screens.content.versions.elements
 
 import android.content.Context
-import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -47,7 +44,6 @@ import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -369,54 +365,47 @@ private fun ModsUpdateEntryItem(
         shape = shape,
         color = color,
         contentColor = contentColor,
+        onClick = {
+            entry.updateSelected(!selected)
+        },
     ) {
         Row(
-            modifier = Modifier.padding(8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            AssetsIcon(
-                modifier = Modifier.clip(shape = RoundedCornerShape(10.dp)),
-                size = 34.dp,
-                iconUrl = data.project.iconUrl
-            )
-
-            Column(
-                modifier = Modifier.weight(1f)
+            Row(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                val title = data.project.title
-                val displayTitle = data.mcMod?.getMcmodTitle(title, context) ?: title
-                Text(
-                    modifier = Modifier.basicMarquee(iterations = Int.MAX_VALUE),
-                    text = displayTitle,
-                    style = MaterialTheme.typography.titleSmall,
-                    maxLines = 1
+                AssetsIcon(
+                    modifier = Modifier.clip(shape = RoundedCornerShape(12.dp)),
+                    size = 52.dp,
+                    iconUrl = data.project.iconUrl
                 )
 
-                //新旧版本对比
-                Row(
-                    modifier = Modifier
-                        .height(IntrinsicSize.Min)
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                Column(
+                    modifier = Modifier.weight(1f)
                 ) {
+                    val title = data.project.title
+                    val displayTitle = data.mcMod?.getMcmodTitle(title, context) ?: title
+                    MarqueeText(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = displayTitle,
+                        style = MaterialTheme.typography.titleSmall,
+                    )
                     //旧版本
                     MarqueeText(
-                        modifier = Modifier.weight(1f),
+                        modifier = Modifier.fillMaxWidth(),
                         text = data.currentVersion ?: "???", //未知
                         style = MaterialTheme.typography.labelSmall.copy(
                             textDecoration = TextDecoration.LineThrough
                         )
                     )
-                    VerticalDivider(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .padding(vertical = 2.dp),
-                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f)
-                    )
                     //新版本
                     MarqueeText(
-                        modifier = Modifier.weight(1f),
+                        modifier = Modifier.fillMaxWidth(),
                         text = newVersion.platformVersion(),
                         style = MaterialTheme.typography.labelSmall
                     )
